56d863be72f043684ba6593fffa8bf0d376b6026
kenichiwaaa <baoanhnguyen.0107@gmail.com>
2022-01-28 11:35:48 +0700

    setup mainnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,28 @@
-
 const hre = require("hardhat");
-const abi = require("../abi/yfiag.abi.json")
+const abi = require("../abi/yfiag.abi.json");
 async function main() {
-  
-
-  const accounts = await hre.ethers.getSigners()
+  const [accounts] = await hre.ethers.getSigners();
   // const Yfiag = await ethers.getContractFactory("YfiagERC20");
   // const yfiag = await ethers.getContractFactory("YfiagERC20");
-  const yfiag = new ethers.Contract('0x71f4A5202A09DA255558F3Fb6D05C8FD24d10a2e', abi, accounts[0]);
+  const yfiag = new ethers.Contract(
+    "0x1F64703ae00C06420dd21fE75E9Ef6E008212263",
+    abi,
+    accounts[0]
+  );
   // const yfiag = await Yfiag.deploy();
   // await yfiag.deployed();
   console.log("YfiagERC20 deployed to:", yfiag.address);
   const DiamondHand = await hre.ethers.getContractFactory("DiamondHand");
-  const master = await DiamondHand.deploy(yfiag.address,"0xAb7D9763ee384C428e752c6A113c520061baD963");
+  const master = await DiamondHand.deploy(yfiag.address, accounts.address);
 
   await master.deployed();
 
   console.log("DiamondHand deployed to:", master.address);
-  let treasury = await yfiag.balanceOf("0xAb7D9763ee384C428e752c6A113c520061baD963");
+  let treasury = await yfiag.balanceOf(accounts.address);
   console.log("treasury balance :", treasury.toString());
-  await yfiag.approve(master.address,treasury);
+  await yfiag.approve(master.address, treasury);
 
-
-  let approval = await yfiag.allowance("0xAb7D9763ee384C428e752c6A113c520061baD963",master.address);
+  let approval = await yfiag.allowance(accounts.address, master.address);
   console.log("approval balance :", approval.toString());
 }
 
